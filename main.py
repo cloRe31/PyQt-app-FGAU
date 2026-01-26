6b7e960b9346edbc274f958b06eefe7b2ec2a059
cloRe31 <vsevolodkrestovskiy@gmail.com>
2026-01-26 18:49:59 +0300

    base interface (допилить)

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QComboBox, QLabel, QLayout
-from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QComboBox, QVBoxLayout, QHBoxLayout, QStackedWidget
+import sys
 from openpyxl import Workbook
 
-class MainWindow(QMainWindow):
+class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
 
-        opertation_combobox = QComboBox()
-        opertation_combobox.addItems(["Поставка", "Расход"])
+        self.resize(900, 600)
+        self.setMinimumSize(600, 400)
+
+        HLayout = QHBoxLayout(self)
+        LVLayout = QVBoxLayout(self)
+        RVLayout = QVBoxLayout(self)
+
+        HLayout.setStretch(0,1)
+        HLayout.setStretch(1,2)
+
+        operation_combobox = QComboBox()
+        operation_combobox.addItems(["Расход", "Поставка"])
+        LVLayout.addWidget(operation_combobox)
         
 
+        Label = QLabel("авироивырп")
+        RVLayout.addWidget(Label)
 
 
+        HLayout.addLayout(LVLayout)
+        HLayout.addLayout(RVLayout)
 
 
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
+
+        
 
 
 
