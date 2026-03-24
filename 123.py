d853373d9d4ed6d05d2d4569a1d5352fa737fa40
cloRe31 <vsevolodkrestovskiy@gmail.com>
2026-03-24 17:10:53 +0300

    testing openpyxl

--- 123.py
+++ 123.py
@@ -1,5 +1,10 @@
 from openpyxl import Workbook, load_workbook
 
-wb = load_workbook(filename = 'Cartriges.xlsx')
-sheet_ranges = wb['']
-print(sheet_ranges['D18'].value)
+wb = load_workbook(filename = 'Data/Cartriges.xlsx')
+sheet_ranges = wb['КАРТРИДЖИ']
+ws = wb.active
+cell_range = ws['A1':'A65']
+for row in ws.iter_rows(min_row=6, max_col=1, max_row=65):
+    for cell in row:
+        print(cell.value)
+
